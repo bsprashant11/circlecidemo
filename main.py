29ba198f94391a4adcff49825c18b97e4248f6ae
unknown <bsatyaprashant@gmail.com>
2022-06-12 02:21:23 +0530

    second time

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running and I am changing something."
+    return "Flask app is running and I am changing something. Well this one to check the CI/CD pipeline deployment"
 
 port = int(os.getenv("PORT", 5001))
 
